948c2f7189ff07b65c20f1e9ecd3b63e119c5f4b
Helvennn <enixgregory@gmail.com>
2025-07-24 04:42:17 +0700

    Page Area Mapped

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
     return(
-        <div>
-            <h1>Welcome, [UserName]</h1>
+        <div className="bg-accent opacity-20 fixed top-[36%] w-screen h-[64%]">
+            <h1 className="absolute left-60 leading-12">Welcome, [UserName]</h1>
         </div>
     );
 }
